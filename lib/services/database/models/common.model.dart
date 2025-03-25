3faa11603059db101494952447bc2f01a81ff687
Yasiru Dharmathilake <yasiru.dharmathilaka@gmail.com>
2025-03-26 00:13:00 +0530

    feat: added common db model

---
+++ lib/services/database/models/common.model.dart
@@ -0,0 +1,16 @@
+Map<String, String> commonColumns = {'id': 'INTEGER PRIMARY KEY'};
+
+abstract class CommonModel {
+  int? id;
+
+  String get tableName => 'common_table';
+  Map<String, String> get columns => commonColumns;
+
+  CommonModel({this.id});
+
+  Map<String, dynamic> toMap() {
+    return {'id': id};
+  }
+
+  CommonModel fromMap(Map<String, dynamic> map);
+}
