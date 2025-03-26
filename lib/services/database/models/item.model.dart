f533b469fa6a310f6ace8e4d7bb9b4ae66b0dda0
Yasiru Dharmathilake <yasiru.dharmathilaka@gmail.com>
2025-03-27 00:06:55 +0530

    feat: added item & item-type models

---
+++ lib/services/database/models/item.model.dart
@@ -0,0 +1,54 @@
+import 'common.model.dart';
+
+class ItemModel extends CommonModel {
+  @override
+  final String tableName = 'item';
+  @override
+  final Map<String, String> columns = {
+    ...commonColumns,
+    'label': 'TEXT',
+    'type_id': 'INTEGER',
+    'color': 'TEXT',
+    'material': 'TEXT',
+    'image_path': 'TEXT',
+  };
+
+  final String label;
+  final int typeId;
+  final String color;
+  final String material;
+  final String imagePath;
+
+  ItemModel({
+    int? id,
+    required this.label,
+    required this.typeId,
+    required this.color,
+    required this.material,
+    required this.imagePath,
+  }) : super(id: id);
+
+  @override
+  Map<String, dynamic> toMap() {
+    return {
+      ...super.toMap(),
+      'label': label,
+      'type_id': typeId,
+      'color': color,
+      'material': material,
+      'image_path': imagePath,
+    };
+  }
+
+  @override
+  ItemModel fromMap(Map<String, dynamic> map) {
+    return ItemModel(
+      id: map['id'],
+      label: map['label'],
+      typeId: map['type_id'],
+      color: map['color'],
+      material: map['material'],
+      imagePath: map['image_path'],
+    );
+  }
+}
