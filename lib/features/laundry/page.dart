2970be41420715a1798b02af0758f318c437b823
Zerone Brianstorm <ometha12@gmail.com>
2025-03-31 19:38:08 +0530

    feat: new interfaces

--- lib/features/laundry/page.dart
+++ lib/features/laundry/page.dart
@@ -1,44 +1,56 @@
 import 'package:flutter/material.dart';
 import '../../components/pill.dart';
 import '../../components/cool-card.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class LaundryView extends StatelessWidget {
+  const LaundryView({super.key});
+
+  @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
       body: Center(
-        child : Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Pill(
-              text: "Calendar",
-              width: 340,
-              height: 285,
+        child: SingleChildScrollView( // Add SingleChildScrollView here
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TableCalendar(
+                firstDay: DateTime.utc(2010, 10, 16),
+                lastDay: DateTime.utc(2030, 3, 14),
+                focusedDay: DateTime.now(),
+                rowHeight: 40.0,
               ),
-            SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Pill(
-                  text: "Today's Weather [Dev]",
-                  width: 160,
-                  height: 100,
-                ),
-                SizedBox(width: 20),
-                Pill(
-                  text: "Today's Wind [Dev]",
-                  width: 160,
-                  height: 100,
-                )
-              ]
-            ),
-            SizedBox(height: 20),
-            Pill(
-              text: "Hourly Weather Container",
-              width: 340,
-              height: 150,
-            )
-          ],
-        )
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Pill(
+                    text: "Rainy ",
+                    weather: "rainy",
+                    width: screenWidth * 0.45,
+                    height: 100,
+                  ),
+                  SizedBox(width: 20),
+                  Pill(
+                    text: "8km/h ",
+                    weather: 'windy',
+                    width: screenWidth * 0.45,
+                    height: 100,
+                  )
+                ],
+              ),
+              SizedBox(height: 20),
+              Pill(
+                text: "",
+                WeatherModeStrings: ["Monday\nsunny", "Tuesday\nwindy", "Wednesday\ncloudy", "Thursday\nsnowy", "Friday\nsunny", "Saturday\nsunny", "Sunday\nrainy"],
+                width: screenWidth * 0.95,
+                height: 150,
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
